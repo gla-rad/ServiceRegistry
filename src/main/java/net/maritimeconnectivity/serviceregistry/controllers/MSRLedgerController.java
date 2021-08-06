971b24098a104c759af9e9740c201c91676c725a
Jinki Jung <your.jinki.jung@gmail.com>
2021-08-06 16:16:31 +0200

    fresh start

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/controllers/MSRLedgerController.java
@@ -0,0 +1,15 @@
+package net.maritimeconnectivity.serviceregistry.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class MSRLedgerController {
+
+
+
+
+}
