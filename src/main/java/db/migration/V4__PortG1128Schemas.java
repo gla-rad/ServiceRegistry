3230a61c60a377e4ad66e455c3b93babe6613517
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-06-02 17:35:21 +0100

    Adding migration but we are missing fields in the porting

--- src/main/java/db/migration/V4__PortG1128Schemas.java
+++ src/main/java/db/migration/V4__PortG1128Schemas.java
@@ -1,10 +1,11 @@
 package db.migration;
 
-import net.maritimeconnectivity.serviceregistry.models.domain.enums.G1128Schemas;
+import net.maritimeconnectivity.eNav.utils.G1128PortingUtils;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -16,12 +17,21 @@ public class V4__PortG1128Schemas extends BaseJavaMigration {
         //Create a new statement to retrieve the current G1128 XML
         final Statement st = connection.createStatement();
         final ResultSet rs = st.executeQuery("SELECT id, content FROM xml;");
+        // Create a statement tp update the XML
+        final PreparedStatement psXML = connection.prepareStatement("UPDATE xml SET content = (?) WHERE id = (?);");
         //For each entry
-//        while (rs.next()) {
-//            final Long xmlId = rs.getLong("id");
-//            final String xml = rs.getString("xml");
-//            G1128Schemas<> (org.iala_aism.g1128.v1_5.serviceinstanceschema.ServiceInstance)
-//        }
+        while (rs.next()) {
+            final Long xmlId = rs.getLong("id");
+            final String xml_v1_3 = rs.getString("content");
+            final String xml_v1_7 = G1128PortingUtils.portXMLVersion_1_3_to_1_7(xml_v1_3);
+
+            // Now perform the update statement
+            psXML.setString(1, xml_v1_7);
+            psXML.setLong(2, xmlId);
+            psXML.execute();
+        }
+        psXML.close();
+        rs.close();
         st.close();
     }
 
