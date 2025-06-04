3c846fad1dd310691ed2e8361c41d3ebc58ba03f
jakobsve <jakob.svenningsen@outlook.com>
2025-06-04 14:36:19 +0200

    Setup MMS Edge router class

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/components/MmsEdgeRouter.java
@@ -0,0 +1,20 @@
+package net.maritimeconnectivity.serviceregistry.components;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * The MMs Edge Router Component
+ *
+ * This component is responsible for sending MMS messages to a designated MMS Router Network
+ * as required by MSR Technical Specifcation Requirement 14.A.
+ * The implementation contains the necessary subset of functionalities (described in RTCM 13900.0) for an edge router
+ * to subscribe and send subject case messages to the MMS Router Network.
+ */
+@Component
+@Slf4j
+public class MmsEdgeRouter {
+
+
+
+}
