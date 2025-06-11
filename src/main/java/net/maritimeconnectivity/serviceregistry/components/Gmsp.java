21b14be30f938cd25df531be59043dd6960fd39b
jakobsve <jakob.svenningsen@outlook.com>
2025-06-11 12:19:06 +0200

    Work on GMSP

--- src/main/java/net/maritimeconnectivity/serviceregistry/components/Gmsp.java
+++ src/main/java/net/maritimeconnectivity/serviceregistry/components/Gmsp.java
@@ -2,18 +2,32 @@ package net.maritimeconnectivity.serviceregistry.components;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import net.maritimeconnectivity.serviceregistry.models.dto.mms.MmsSearchMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class Gmsp {
 
     @Autowired
     ObjectMapper objectMapper;
 
+    private final MmsEdgeRouter mmsEdgeRouter;
+
+    public Gmsp(MmsEdgeRouter er) {
+        this.mmsEdgeRouter = er;
+    }
 
 
     public void globalSearch (MmsSearchMessageDto mmsSearchMessageDto) {
-        // TODO : Write JSON Object
+        try {
+            String searchMessageJson = writeJsonSearchMessage(mmsSearchMessageDto);
+        } catch (JsonProcessingException e) {
+            log.error("Error writing JSON for MmsSearchMessageDto", e);
+        }
+
         // Send Via MMS Edge Router
     }
 
@@ -21,6 +35,8 @@ public class Gmsp {
         return objectMapper.writeValueAsString(mmsSearchMessageDto);
     }
 
+
+    //Somefunction that can call a callback to client code upon event
 
 
 }
