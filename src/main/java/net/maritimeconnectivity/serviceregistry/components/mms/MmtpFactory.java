68c6d8f78742be19bd2bb8e77de0e8fea432a51e
jakobsve <jakob.svenningsen@outlook.com>
2025-06-11 12:26:12 +0200

    Add Mmtp Factory interface

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/components/mms/MmtpFactory.java
@@ -0,0 +1,30 @@
+package net.maritimeconnectivity.serviceregistry.components.mms;
+import net.maritimeconnectivity.mmtp.MmtpMessage;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.List;
+
+@Component
+public interface MmtpFactory {
+
+    MmtpMessage createConnectMessage();
+
+    MmtpMessage createSendMessage(
+            String subject,
+            String sender,
+            String body,
+            Duration ttl
+    );
+
+    MmtpMessage createSendMessageMrn(
+            List<String> recipients,
+            String sender,
+            String body,
+            Duration ttl
+    );
+
+    MmtpMessage createReceiveMessage();
+
+    MmtpMessage createDisconnectMessage();
+}
