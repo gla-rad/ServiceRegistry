577c249922b6b8e83dcf238ea64fe5b2a349e790
jakobsve <jakob.svenningsen@outlook.com>
2025-06-24 14:16:50 +0200

    Add constant for global search subj

--- src/main/java/net/maritimeconnectivity/serviceregistry/components/Gmsp.java
+++ src/main/java/net/maritimeconnectivity/serviceregistry/components/Gmsp.java
@@ -3,39 +3,73 @@ package net.maritimeconnectivity.serviceregistry.components;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import net.maritimeconnectivity.mmtp.MmtpMessage;
 import net.maritimeconnectivity.serviceregistry.components.mms.MmsEdgeRouter;
+import net.maritimeconnectivity.serviceregistry.components.mms.MmtpFactory;
 import net.maritimeconnectivity.serviceregistry.models.dto.mms.MmsSearchMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
 
 @Component
 @Slf4j
 public class Gmsp {
+
+    @Value("${info.gmsp.search.globalSubject}")
+    private String globalSearchSubject;
 
     @Autowired
     ObjectMapper objectMapper;
 
     private final MmsEdgeRouter mmsEdgeRouter;
 
-    public Gmsp(MmsEdgeRouter er) {
+    private final MmtpFactory mmtpFactory;
+
+    public Gmsp(MmsEdgeRouter er, MmtpFactory mmtpFactory) {
         this.mmsEdgeRouter = er;
+        this.mmtpFactory = mmtpFactory;
+
+        // Create mmtpFactory and register it with the MmsEdgeRouter
+
     }
 
 
     public void globalSearch (MmsSearchMessageDto mmsSearchMessageDto) {
         try {
+            // Convert MmsSearchMessageDto to JSON string
             String searchMessageJson = writeJsonSearchMessage(mmsSearchMessageDto);
+
+            //Set TTL to 1 h. Note this is a high level java object and not seconds after epoch
+            Duration ttl = Duration.ofHours(1);
+
+            //Calculate subject based on whether geometry is present
+            // TODO: Implement this, but we need to have postgis calculate the intersections of the geometry
+
+            MmtpMessage msg = this.mmtpFactory.createSendMessage(
+                    globalSearchSubject,
+                    mmsSearchMessageDto.getConsumerMRN(),
+                    searchMessageJson,
+                    ttl
+            );
+
+            mmsEdgeRouter.sendMessage(msg);
+
+
         } catch (JsonProcessingException e) {
             log.error("Error writing JSON for MmsSearchMessageDto", e);
+        } catch (IOException e) {
+            log.error("Error sending message via MmsEdgeRouter", e);
         }
-
-        // Send Via MMS Edge Router
-
     }
 
     private String writeJsonSearchMessage(MmsSearchMessageDto mmsSearchMessageDto) throws JsonProcessingException {
         return objectMapper.writeValueAsString(mmsSearchMessageDto);
     }
+
+
 
 
     //Somefunction that can call a callback to client code upon event
