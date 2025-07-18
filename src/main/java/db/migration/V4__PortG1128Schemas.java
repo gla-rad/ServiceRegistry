b461bf68c99f9c82b5479b9ea5b064be40a12a95
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-07-18 12:14:59 +0100

    Added comments and checksum to java migration

--- src/main/java/db/migration/V4__PortG1128Schemas.java
+++ src/main/java/db/migration/V4__PortG1128Schemas.java
@@ -21,14 +21,45 @@ import net.maritimeconnectivity.eNav.utils.G1128PortingUtils;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.zip.CRC32;
 
+/**
+ * The G1128 Schema Migration
+ * <p/>
+ * Implements the G1128 migration which is 4th in line to be executed. This
+ * migration will port the old G1128 v1.3 existing service registrations to
+ * the latest v1.7.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
 @Slf4j
-public class V4__PortG1128Schemas extends BaseJavaMigration {
+public class V4__PortG1128Schemas extends BaseJavaMigration implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 4569511869080087442L;
+
+    /**
+     * Implements the migration process using Java to take advantage of
+     * the G1128  library facilities that can translate the old G1128
+     * version 1.3 to  the latest version 1.7.
+     * <p/>
+     * The process is as follows:
+     * <ul>
+     *    <li>1. The migration will read all existing G1128 XML-base registrations, including the XML content.</li>
+     *    <li>2. The migration will use the G1128PortingUtils utility to upgrade the XML content.</li>
+     *    <li>3. The generated results will then be stored to the database.</li>
+     * </ul>
+     * Any errors that occur will not stop the migration, and will just get logged.
+     *
+     * @param context the Flyway Context
+     * @throws Exception for any exceptions thrown during the operation
+     */
     @Override
     public void migrate(Context context) throws Exception {
         final Connection connection = context.getConnection();
@@ -57,6 +88,20 @@ public class V4__PortG1128Schemas extends BaseJavaMigration {
         psXML.close();
         rs.close();
         st.close();
+    }
+
+    /**
+     * Returns a checksum for this migration.
+     * <p/>
+     * CAREFULL!!!
+     * The checksum fixed based on the serialization UID of the class. You will
+     * need to manually change it if anything is altered in the code.
+     *
+     * @return the migration checksum
+     */
+    @Override
+    public Integer getChecksum() {
+        return (int) serialVersionUID % Integer.MAX_VALUE + 1;
     }
 
 }
