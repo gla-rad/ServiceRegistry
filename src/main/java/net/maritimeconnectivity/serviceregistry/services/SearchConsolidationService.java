30d50da20c2e892268af26742149a7ffc6df79a7
jakob22r <jakob.svenningsen@outlook.com>
2025-08-28 10:50:25 +0200

    Setup class for search consolidation

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/services/SearchConsolidationService.java
@@ -0,0 +1,19 @@
+package net.maritimeconnectivity.serviceregistry.services;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service implementation for consolidating search results obtained from local and global search
+ *
+ * @author Jakob Svenningsen (email: jakob@dmc.international)
+ */
+@Service
+@Slf4j
+public class SearchConsolidationService {
+
+
+
+
+}
