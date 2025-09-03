b27042cc43e760654cb231eec9600b0e0b1e07b2
jakob22r <jakob.svenningsen@outlook.com>
2025-09-03 11:10:28 +0200

    Initial draft of RetrieveResultsControllere

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/controllers/secom/v2/RetrieveResultsController.java
@@ -0,0 +1,56 @@
+package net.maritimeconnectivity.serviceregistry.controllers.secom.v2;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import lombok.extern.slf4j.Slf4j;
+import net.maritimeconnectivity.serviceregistry.services.SearchConsolidationService;
+import org.grad.secomv2.core.base.SecomConstants;
+import org.grad.secomv2.core.interfaces.GenericSecomInterface;
+import org.grad.secomv2.core.models.SearchObjectResult;
+import org.grad.secomv2.core.models.SearchResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Component
+@Path("/")
+@Slf4j
+@Validated
+public class RetrieveResultsController implements GenericSecomInterface {
+
+    /**
+     * The Interface Endpoint Path.
+     */
+    static final String RETREIVE_RESULTS_INTERFACE_PATH = "/" + SecomConstants.SECOM_VERSION + "/retrieveResults";
+
+    @Autowired
+    SearchConsolidationService searchConsolidationService;
+
+    @Path(RETREIVE_RESULTS_INTERFACE_PATH + "/{transactionId}")
+    @GET
+    @Produces("application/json")
+    public SearchResult retrieveResults(@PathParam("transactionId") String transactionId, @Context final HttpServletResponse response) {
+
+        List<SearchObjectResult> services = searchConsolidationService.getResults(transactionId);
+
+        //If empty, return 404
+        if (services.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+
+        //Wrap in SearchResult
+        SearchResult searchResult = new SearchResult();
+        searchResult.setTransactionId(transactionId);
+        searchResult.setServices(services);
+        return searchResult;
+    }
+
+
+}
