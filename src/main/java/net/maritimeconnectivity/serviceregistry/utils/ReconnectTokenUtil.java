edf39e892268ff72e4aee86344bfc95669946e18
jakob22r <jakob.svenningsen@outlook.com>
2025-10-16 10:41:07 +0200

    Add fileutil for RW of reconnectToken

--- src/main/java/net/maritimeconnectivity/serviceregistry/utils/ReconnectTokenUtil.java
+++ src/main/java/net/maritimeconnectivity/serviceregistry/utils/ReconnectTokenUtil.java
@@ -8,19 +8,21 @@ import java.io.*;
  *
  * @author Jakob Svenningsen (email: jakob@dmc.international)
  */
-public final class FileIOUtil {
+public final class ReconnectTokenUtil {
 
-    private FileIOUtil() {
+    private static final String filename = "rc_token.txt";
+
+    private ReconnectTokenUtil() {
         throw new UnsupportedOperationException("Utility class should not be instantiated");
     }
 
-    public static void writeStringToCurrentDir(String filename, String text) throws IOException {
+    public static void writeRcToken(String text) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             writer.write(text);
         }
     }
 
-    public static String readStringFromCurrentDir(String filename) throws IOException {
+    public static String readRcToken() throws IOException {
         StringBuilder content = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
