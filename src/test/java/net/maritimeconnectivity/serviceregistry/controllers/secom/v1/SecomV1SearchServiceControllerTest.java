9da234b753693733f7aa0b540ba0197c95c36ce7
jakob22r <jakob.svenningsen@outlook.com>
2025-12-02 10:53:58 +0100

    Fix call to changed function signature

--- src/test/java/net/maritimeconnectivity/serviceregistry/controllers/secom/v1/SecomV1SearchServiceControllerTest.java
+++ src/test/java/net/maritimeconnectivity/serviceregistry/controllers/secom/v1/SecomV1SearchServiceControllerTest.java
@@ -78,9 +78,6 @@ class SecomV1SearchServiceControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    public DomainDtoMapper<?,?> searchObjectResultMapper;
-
     @MockitoBean
     private InstanceService instanceService;
 
@@ -167,7 +164,7 @@ class SecomV1SearchServiceControllerTest {
         Page<Instance> page = new PageImpl<>(this.instances, this.pageable, this.instances.size());
 
         // Mock the service call for creating a new instance
-        doReturn(page).when(this.instanceService).handle(any(), any(), any());
+        doReturn(page).when(this.instanceService).handle(any(), any(), any(), any());
 
         // Perform the web request
         webTestClient.post()
@@ -220,7 +217,7 @@ class SecomV1SearchServiceControllerTest {
         Page<Instance> page = new PageImpl<>(this.instances, this.pageable, this.instances.size());
 
         // Mock the service call for creating a new instance
-        doReturn(page).when(this.instanceService).handle(any(), any(), any());
+        doReturn(page).when(this.instanceService).handle(any(), any(), any(), any());
         doAnswer(i -> this.mcpServiceDtos.get(i.getArguments()[1])).when(this.mirClient).getServiceEntity(any(), any(), any());
         doAnswer(i -> this.mcpServiceDtos.get((String)i.getArgument(1))).when(this.mirClient).getServiceEntity(any(), any(), any());
 
@@ -288,7 +285,7 @@ class SecomV1SearchServiceControllerTest {
         Page<Instance> page = new PageImpl<>(this.instances, this.pageable, this.instances.size());
 
         // Mock the service call for creating a new instance
-        doReturn(page).when(this.instanceService).handle(any(), any(), any());
+        doReturn(page).when(this.instanceService).handle(any(), any(), any(), any());
 
         // Perform the web request
         webTestClient.post()
@@ -340,7 +337,7 @@ class SecomV1SearchServiceControllerTest {
         Page<Instance> page = new PageImpl<>(this.instances, this.pageable, this.instances.size());
 
         // Mock the service calls for creating a new instance
-        doReturn(page).when(this.instanceService).handle(any(), any(), any());
+        doReturn(page).when(this.instanceService).handle(any(), any(), any(), any());
         doAnswer(i -> this.mcpServiceDtos.get((String)i.getArgument(1))).when(this.mirClient).getServiceEntity(any(), any(), any());
 
         // Perform the web request
