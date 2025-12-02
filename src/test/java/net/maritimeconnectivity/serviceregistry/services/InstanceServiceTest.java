92ff048cc162108985ad9da206968848b4ac8314
jakob22r <jakob.svenningsen@outlook.com>
2025-12-02 10:51:35 +0100

    Fixed call to changed function type

--- src/test/java/net/maritimeconnectivity/serviceregistry/services/InstanceServiceTest.java
+++ src/test/java/net/maritimeconnectivity/serviceregistry/services/InstanceServiceTest.java
@@ -668,7 +668,7 @@ class InstanceServiceTest {
         doReturn(mockedQuery).when(this.instanceService).getSearchInstanceQueryByQueryString(any(), any(), any());
 
         // Perform the service call
-        Page<Instance> result = this.instanceService.handle("search-field:search-value", this.point, this.pageable);
+        Page<Instance> result = this.instanceService.handle("search-field:search-value", this.point, this.pageable, false);
 
         // Validate the result
         assertNotNull(result);
