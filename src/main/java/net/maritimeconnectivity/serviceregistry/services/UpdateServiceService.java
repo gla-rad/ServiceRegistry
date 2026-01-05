125e5b391d1c417aa87d1b47ffeff389d6c5c9e1
jakob22r <jakob.svenningsen@outlook.com>
2026-01-05 13:29:04 +0100

    Add UpdateServiceService class

---
+++ src/main/java/net/maritimeconnectivity/serviceregistry/services/UpdateServiceService.java
@@ -0,0 +1,25 @@
+package net.maritimeconnectivity.serviceregistry.services;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service Implementation G1191 defined updateService interface.
+ *
+ * @author Jakob Svenningsen (email: jakob@dmc.international)
+ */
+@Service
+@Slf4j
+public class UpdateServiceService {
+
+    //Constructor
+    @Autowired
+    public UpdateServiceService(InstanceService instanceService) {
+        // Initialization code if needed
+    }
+
+
+
+}
