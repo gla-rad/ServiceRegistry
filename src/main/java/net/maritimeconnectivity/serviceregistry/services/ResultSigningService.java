8394965e3917d74f467253e917c78cdee27ff5e7
jakob22r <jakob.svenningsen@outlook.com>
2026-03-23 10:13:47 +0100

    Change name to ResultSigningService

--- src/main/java/net/maritimeconnectivity/serviceregistry/services/ResultSigningService.java
+++ src/main/java/net/maritimeconnectivity/serviceregistry/services/ResultSigningService.java
@@ -4,15 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
- * Service implementation for validating search results obtained from local and global search
+ * Service implementation for signing search results obtained from local and global search
  * Checks conducted are described in IALA G1191
  * Note - that invalid signatures have already been rejected by the middleware, so this service
- * focuses on validating the content of the results, not the signature!
+ * focuses on signing the results to the client.
  * @author Jakob Svenningsen (email: jakob@dmc.international)
  */
 @Service
 @Slf4j
-public class ResultValidationService {
+public class ResultSigningService {
 
 
 }
